3c913fdee9260f04404fe86fd5d0ec74aae51880
1bombik_ <dima.vezh14@mail.ru>
2023-02-08 20:41:24 +0300

    Initial

--- leetcode_20.py
+++ leetcode_20.py
@@ -1,10 +1,15 @@
+# https://leetcode.com/problems/valid-parentheses/description/
+
 class Solution:
     def isValid(self, s: str) -> bool:
-        while '()' in s or '[]' in s or "{}" in s:
-            s = s.replace('()', '')
-            s = s.replace('{}', '')
-            s = s.replace('[]', '')
-        if len(s) > 0:
+        if len(s) % 2 != 0:
             return False
         else:
-            return True
+            while '()' in s or '[]' in s or "{}" in s:
+                s = s.replace('()', '')
+                s = s.replace('{}', '')
+                s = s.replace('[]', '')
+            if len(s) > 0:
+                return False
+            else:
+                return True
