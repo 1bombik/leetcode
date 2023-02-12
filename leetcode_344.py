de4c2caef0c6f94e797081bee55f9a721815c367
1bombik_ <dima.vezh14@mail.ru>
2023-02-12 14:29:47 +0300

    Initial

---
+++ leetcode_344.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/reverse-string/description/?envType=study-plan&id=algorithm-i
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        return s.reverse()
