5267812a72fd73f25b8293b2d81800e2f20246db
1bombik_ <dima.vezh14@mail.ru>
2023-03-28 14:16:53 +0300

    Initial

---
+++ leetcode_771.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/jewels-and-stones/description/
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        answer = 0
+        for j in jewels:
+            for s in stones:
+                if j == s:
+                    answer += 1
+        return answer
