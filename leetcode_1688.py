64a7fbe5f1f26bbc58e4e8977fcf7dc362e75b29
1bombik_ <dima.vezh14@mail.ru>
2023-04-04 23:38:11 +0300

    Initial

---
+++ leetcode_1688.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/count-of-matches-in-tournament/description/
+
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        matches = 0
+        while n != 1:
+            if n % 2 == 0:
+                n //= 2
+                matches += n
+            else:
+                n = (n + 1) // 2
+                matches += n - 1
+        return matches
